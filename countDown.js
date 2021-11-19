13ea88f43fb9e92d29b704067126f3edcae3fdf7
evgeny-chernousov <evgeny.cea@gmail.com>
2021-11-19 22:04:24 +0200

    added multiple tasks support

--- countDown.js
+++ countDown.js
@@ -1,11 +1,14 @@
-taskName = '';
-taskEndDate = '';
-telegramToken = '';
-telegramChatId = '';
+var config = require("./countDownConfig.json")
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(config.telegramBotToken, {polling: false});
+
+message = '';
 messagesRandom = [
     'What are you waiting for? ',
     'It is not funny. ',
-    'Just do it! '
+    'Just do it! ',
+    'Vypestovivay!',
+    'Seriously, it is not rocket science! '
 ]
 
 class Task {
@@ -16,27 +19,27 @@ class Task {
     getDaysLeft() {
         const currentDate = new Date(Date.now());
         const completeDate = new Date(this.completeDate);
-        const diffTime = Math.abs(completeDate - currentDate);
+        const diffTime = (completeDate - currentDate);
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
         return diffDays;
     }
 }
 
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(telegramToken, {polling: false});
-
-function getRandomInt(min, max) {
+function getRandomInt(min, max) { //Getting random integer value between min and max
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; 
 }
 
-let task = new Task(taskName, taskEndDate)
-message = 'Task: ' + task.name + '. Days left: ' + task.getDaysLeft() + '. ';
-
-if (task.getDaysLeft() > 11){
-    bot.sendMessage(telegramChatId, message);
-}
-else{
-    bot.sendMessage(telegramChatId, message + messagesRandom[getRandomInt(0, messagesRandom.length)] + 'Hurry up!');
-}
+config.tasks.forEach(task => {  
+    if (task.isEnabled == 0){
+        return;
+    }  
+    let taskObj = new Task(task.taskName, task.taskEndDate)
+    if(taskObj.getDaysLeft() < 1){
+        console.log('Time already passed, exit');
+        return;
+    }
+    message += '\n\nTask: ' + taskObj.name + '. Days left: ' + taskObj.getDaysLeft() + '. ';
+});
+bot.sendMessage(config.telegramChatId, message + '\n\n' + messagesRandom[getRandomInt(0, messagesRandom.length)]);
